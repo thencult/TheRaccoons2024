c73d0d66bb802c541d7710c559c31e4a49e4ff01
Ēriks <e@boka.lv>
2024-11-17 13:28:14 +0200

    Tasks descriptions

--- Assets/_Scripts/Tasks/Task.cs
+++ Assets/_Scripts/Tasks/Task.cs
@@ -8,16 +8,24 @@ public class Task : MonoBehaviour
     public TaskData taskData;
     public TextMeshProUGUI title;
     public TextMeshProUGUI description;
+/*
+    public GameObject titleObject;
+    public GameObject descriptionObject;*/
 
     private void Awake()
     {
-        title = GetComponent<TextMeshProUGUI>();
-        description = GetComponent<TextMeshProUGUI>();
-    }
+        title = GetComponentsInChildren<TextMeshProUGUI>()[0];
+        description = GetComponentsInChildren<TextMeshProUGUI>()[1];
 
+    }
+    private void Update()
+    {
+        AssignText();
+    }
     public void AssignText()
     {
-
+        title.text = taskData.name;
+        description.text = taskData.description;
     }
     
     public void ExecuteTask()
